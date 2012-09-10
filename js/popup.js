5c9d0cfa66b4a1d1ceb700c63edd0797b5d627c7
Robert Pataki <heartcode@robertpataki.com>
2012-09-11 00:02:32 +0100

    Analytics code refined.

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,27 @@
 if (typeof Fontier == "undefined" || !Fontier) {var Fontier = {}};
 
 var ft = Fontier;
+ft.analyticsCode = 'UA-19326313-14';
 
-ft.handleClick = function() {
+ft.handleClick = function(e) {
   if (!$(this).hasClass('selected')) {
     var size = parseInt($(this).data('size'));
-
     chrome.experimental.fontSettings.setDefaultFontSize({pixelSize: size});
-
     $("#font_size_list li.selected").removeClass('selected');
     $(this).addClass('selected');
+
+    // Tracking the default font size
+    _gaq.push(['_trackEvent', 'setFontSize', size]);
   }
 };
 
 ft.getDefaultFontSize = function() {
-  console.log("getDefaultFontSize!");
   chrome.experimental.fontSettings.getDefaultFontSize({}, ft.setDefaultFontSize);
 };
-
 ft.setDefaultFontSize = function(font) {
+  // Tracking the default font size
+  _gaq.push(['_trackEvent', 'getFontSize', font.pixelSize]);
+
   $("#font_size_list li").each(function(index, item) {
     if (parseInt($(item).data('size')) == font.pixelSize) {
       $(this).addClass('selected');
@@ -30,3 +33,16 @@ $(document).ready(function() {
   ft.getDefaultFontSize();
   $("#font_size_list li").on('click', ft.handleClick);
 });
+
+/*
+* Google Analytics tracking code
+*/
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', ft.analyticsCode]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
